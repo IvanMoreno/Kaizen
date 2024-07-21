9ae993f54457d5e79d091c0b7ba0fdda17722106
IvanMoreno <ivanmorenogarciafresneda@gmail.com>
2024-07-21 11:40:53 +0200

    feat!: introduce workday notion

---
+++ lib/Workday.dart
@@ -0,0 +1,8 @@
+class Workday {
+  final DateTime date;
+  final int rating;
+  final List<GoodThing> goodThings;
+  final List<BadThing> badThings;
+
+  Workday.name(this.date, this.rating, this.goodThings, this.badThings);
+}
--- test/WorkdayFormulary_test.dart
+++ test/WorkdayFormulary_test.dart
@@ -18,8 +18,8 @@ import 'package:kaizen/WorkdayFormulary.dart';
 void main() {
   testWidgets('All formulary fields are empty by default',
       (WidgetTester tester) async {
-    await tester
-        .pumpWidget(EmbedInApp(WorkdayFormulary(() => DateTime(2024, 7, 1),MockRepository())));
+    await tester.pumpWidget(EmbedInApp(
+        WorkdayFormulary(() => DateTime(2024, 7, 1), MockRepository())));
 
     expect(find.text("No Rating"), findsOne);
     expect(find.text("July 01, 2024"), findsOne);
@@ -31,11 +31,10 @@ void main() {
 
   testWidgets('Upload filled formulary to repository',
       (WidgetTester tester) async {
-    
-    var mockRepository = MockRepository();
-    
-    await tester
-        .pumpWidget(EmbedInApp(WorkdayFormulary(() => DateTime(2024, 7, 1), mockRepository)));
+    var mock = MockRepository();
+
+    await tester.pumpWidget(
+        EmbedInApp(WorkdayFormulary(() => DateTime(2024, 7, 1), mock)));
 
     var ratingDropdown = find.byKey(const ValueKey("RatingDropdown"));
     await tester.tap(ratingDropdown);
@@ -45,17 +44,27 @@ void main() {
     await tester.pumpAndSettle();
 
     await tester.enterText(find.byType(GoodThingField).first, "something good");
-    
+
     await tester.tap(find.byKey(const Key("AddBadThing")));
     await tester.pumpAndSettle();
     await tester.enterText(find.byType(BadThingField).first, "something bad");
-    await tester.enterText(find.byType(ProposedSolutionField).first, "a solution");
+    await tester.enterText(
+        find.byType(ProposedSolutionField).first, "a solution");
 
     await tester.tap(find.byKey(const Key("EndWorkday")));
+
+    expect(
+        mock.LastSubmission,
+        Workday(date: DateTime(2024, 7, 1), rating: 1, goodThings: [
+          GoodThing("something good")
+        ], badThings: [
+          BadThing(issue: "something bad", solution: "a solution")
+        ]));
   });
 }
 
 class MockRepository implements WorkdaysRepository {
+  var LastSubmission;
 }
 
 MaterialApp EmbedInApp(Widget widget) =>
