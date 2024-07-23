ee2ccf64daea32cf7e81652676e1f5405dc34af4
IvanMoreno <ivanmorenogarciafresneda@gmail.com>
2024-07-23 18:20:16 +0200

    feat: save workdays in local memory

--- lib/Infrastructure/LocalRepository.dart
+++ lib/Infrastructure/LocalRepository.dart
@@ -1,15 +1,29 @@
+import 'dart:convert';
+
 import 'package:kaizen/Application/WorkdayRepository.dart';
 import 'package:kaizen/Domain/Workday.dart';
+import 'package:kaizen/Infrastructure/WorkdaySerializer.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-class LocalRepository implements WorkdaysRepository
-{
+class LocalRepository implements WorkdaysRepository {
+  SharedPreferences? preferences;
+  List<Workday> allWorkdays = List.empty(growable: true);
+
   @override
-  void Save(Workday today) {
-    throw UnimplementedError();
+  void Save(Workday today) async {
+    preferences ??= await SharedPreferences.getInstance();
+
+    allWorkdays.add(today);
+    preferences!.setString('history', jsonEncode(WorkdaySerializer.ToJson(today)));
   }
 
   @override
-  Future<List<Workday>> Load() {
-    throw UnimplementedError();
+  Future<List<Workday>> Load() async {
+    preferences ??= await SharedPreferences.getInstance();
+    if(allWorkdays.isEmpty && preferences!.containsKey('history')) {
+      allWorkdays = WorkdaySerializer.FromJsonAll(jsonDecode(preferences!.getString('history')!));
+    }
+    
+    return allWorkdays;
   }
 }
